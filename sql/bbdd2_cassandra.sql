e2761ffb90518270f476cce9e61e0f31d43de3f2
Jorge de la Rosa <76626325+Mixnikon108@users.noreply.github.com>
2024-03-09 18:38:10 +0100

    Update bbdd2_cassandra.sql

--- sql/bbdd2_cassandra.sql
+++ sql/bbdd2_cassandra.sql
@@ -1,4 +1,4 @@
--- SQL!!
+SQL!!
 
 --  para un país concreto se muestran para cada mazmorra del juego todos los jugadores, incluyendo sus tiempos
 SELECT email, country, idD, lowest_time, date, name, indx, userName
@@ -24,7 +24,21 @@ INTO OUTFILE '/var/lib/mysql-files/Statistics.csv'
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
 
--- CASSANDRA!!
+
+-- Leaderboar hordas
+SELECT WebUser.country, Kills.idE, WebUser.userName, WebUser.email, COUNT(*) n_kills
+FROM Kills
+JOIN WebUser ON Kills.email = WebUser.email
+GROUP BY Kills.idE, WebUser.country, WebUser.userName, WebUser.email
+ORDER BY WebUser.country ASC, Kills.idE ASC, n_kills DESC, WebUser.userName ASC
+INTO OUTFILE '/var/lib/mysql-files/Hordas.csv'
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
+
+
+
+
+CASSANDRA!!
 cqlsh --request-timeout=10000
 CREATE KEYSPACE Dungeons WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 3}
 USE dungeons;
@@ -32,26 +46,32 @@ CONSISTENCY ALL;
 
 CREATE TABLE hall_of_fame (
     country VARCHAR,
-    dungeon_id INT,
+    dungeon_id TINYINT,
     dungeon_name VARCHAR,
     email VARCHAR,
     username VARCHAR,
-    lowest_time INT,
+    lowest_time TINYINT,
     date TIMESTAMP,
     PRIMARY KEY (country, dungeon_id, lowest_time, email)
 );
 
 COPY hall_of_fame (email, country, dungeon_id, lowest_time, date, dungeon_name, username) FROM '/var/lib/cassandra/HallofFame.csv' WITH HEADER = false;
 
+
+
 CREATE TABLE player_statistics (
+    country VARCHAR,
+    dungeon_id TINYINT,
     email VARCHAR,
-    dungeon_id INT,
-    lowest_time INT,
+    lowest_time TINYINT,
     date TIMESTAMP,
-    PRIMARY KEY (email, dungeon_id, date)
+    PRIMARY KEY (country, dungeon_id, lowest_time, email)
 );
 
-COPY player_statistics (email, dungeon_id, lowest_time, date) FROM '/var/lib/cassandra/Statistics.csv' WITH HEADER = false;
+COPY player_statistics (email, dungeon_id, lowest_time, date) FROM '/var/lib/cassandra/stats.csv' WITH HEADER = false;
+
+
+
 
 CREATE TABLE hordas(
     country VARCHAR,
@@ -63,8 +83,3 @@ CREATE TABLE hordas(
 ) WITH CLUSTERING ORDER BY (n_kills DESC, email ASC);
 
 COPY hordas (country, event_id, username, email, n_kills) FROM '/var/lib/cassandra/hordas.csv' WITH HEADER = false;
-
-
-
-
-
