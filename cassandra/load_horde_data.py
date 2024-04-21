31dac0a0597548444fee186de2230e57bdb5d054
Pabloo22 <pablete.arino@gmail.com>
2024-04-21 14:21:59 +0200

    [Cassandra] Update load_horde_data.py to create keyspace and table if not exists

--- cassandra/load_horde_data.py
+++ cassandra/load_horde_data.py
@@ -7,65 +7,67 @@ from cassandra.cluster import Cluster
 def main():
     cluster = Cluster(["localhost"], port=9042)
 
-    # Create keyspace and table
-    session = cluster.connect()
-    session.execute(
+    try:
+        # Create keyspace and table
+        session = cluster.connect()
+        session.execute(
+            """
+            CREATE KEYSPACE IF NOT EXISTS dungeons
+            WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 2}
         """
-        CREATE KEYSPACE IF NOT EXISTS dungeon
-        WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 2}
-    """
-    )
+        )
 
-    session.set_keyspace("dungeon")
+        session.set_keyspace("dungeons")
 
-    # Create table if not exists
-    # session.execute(
-    #     """
-    #     CREATE TABLE hordas(
-    #         country VARCHAR,
-    #         event_id INT,
-    #         username VARCHAR,
-    #         email VARCHAR,
-    #         n_kills COUNTER,
-    #         PRIMARY KEY ((country, event_id), email, username)
-    #     );
-    # """
-    # )
-
-    # insert_stmt = session.prepare(
-    #     """
-    #     INSERT INTO hordas (country, event_id, email, username)
-    #     VALUES (?, ?, ?, ?)
-    # """
-    # )
-
-    update_counter_stmt = session.prepare(
+        # Create table if not exists
+        session.execute(
+            """
+            CREATE TABLE IF NOT EXISTS hordas(
+                country VARCHAR,
+                event_id INT,
+                username VARCHAR,
+                email VARCHAR,
+                n_kills COUNTER,
+                PRIMARY KEY ((country, event_id), email, username)
+            );
         """
-        UPDATE hordas SET n_kills = n_kills + ?
-        WHERE country = ? AND event_id = ? AND email = ? AND username = ?
-    """
-    )
+        )
 
-    current_file_path = os.path.dirname(os.path.abspath(__file__))
-    csv_file_path = os.path.join(current_file_path, "export/Hordas.csv")
+        # insert_stmt = session.prepare(
+        #     """
+        #     INSERT INTO hordas (country, event_id, email, username)
+        #     VALUES (?, ?, ?, ?)
+        # """
+        # )
 
-    with open(csv_file_path, "r", encoding="utf-8") as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            country, event_id, username, email, n_kills = row
-            event_id = int(event_id)
-            n_kills = int(n_kills)
+        update_counter_stmt = session.prepare(
+            """
+            UPDATE hordas SET n_kills = n_kills + ?
+            WHERE country = ? AND event_id = ? AND email = ? AND username = ?
+        """
+        )
 
-            # Insert data except the counter
-            # session.execute(insert_stmt, (country, event_id, email, username))
+        current_file_path = os.path.dirname(os.path.abspath(__file__))
+        csv_file_path = os.path.join(current_file_path, "export/Hordas.csv")
 
-            # Update the counter
-            session.execute(
-                update_counter_stmt, (n_kills, country, event_id, email, username)
-            )
+        with open(csv_file_path, "r", encoding="utf-8") as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                country, event_id, username, email, n_kills = row
+                event_id = int(event_id)
+                n_kills = int(n_kills)
 
-    print("Data import completed successfully.")
-    cluster.shutdown()
+                # Insert data except the counter
+                # session.execute(insert_stmt, (country, event_id, email, username))
+
+                # Update the counter
+                session.execute(
+                    update_counter_stmt, (n_kills, country, event_id, email, username)
+                )
+
+        print("Data import completed successfully.")
+    finally:
+        cluster.shutdown()
 
 
 if __name__ == "__main__":
