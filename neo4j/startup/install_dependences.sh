a356266bea396c5b1beb6c40875ccb802057a88d
Pabloo22 <pablete.arino@gmail.com>
2024-04-21 19:20:25 +0200

    [Neo4j] Update dependencies in install_dependences.sh

--- neo4j/startup/install_dependences.sh
+++ neo4j/startup/install_dependences.sh
@@ -1,1 +1,18 @@
-pip install neo4j yfiles_jupyter_graphs networkx ipywidgets pyvis matplotlib seaborn graphdatascience scipy numpy pandas plotly regraph
+apt update
+apt-get install graphviz -y
+
+pip install neo4j \
+yfiles_jupyter_graphs \
+networkx \
+ipywidgets \
+pyvis \
+matplotlib \
+seaborn \
+graphdatascience \
+scipy \
+numpy \
+pandas \
+plotly \
+regraph \
+netgraph \
+pydot
